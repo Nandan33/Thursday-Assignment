ff12c661dd52594370d62d46609f051a3df5cad0
Nandan33 <115688247+Nandan33@users.noreply.github.com>
2022-10-21 14:47:47 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class Increment {
+ public static void main(String[] args) {
+	 
+	 int val1=10;
+	 int val2=20;
+	 
+	 // incrementing both pre and post fix
+	System.out.println("incrementing both pre and post fix");
+	System.out.println("pre-incrementing val1 :  "+ ++val1);
+	System.out.println("pre-incrementing val2 :  "+ ++val2);
+	System.out.println("post-incrementing val1 :  "+ val1++);
+	System.out.println("post-incrementing val2 :  "+ val2++);
+}
+}
