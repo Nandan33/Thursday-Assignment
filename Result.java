f2c57c42d67e8b4ecf3089f09cd52a703c8adea8
Nandan33 <115688247+Nandan33@users.noreply.github.com>
2022-10-27 14:00:19 +0530

    Add files via upload
    Thursday Assignment 27-10-2022

---
+++ Result.java
@@ -0,0 +1,16 @@
+package ta2;
+
+
+
+public class Result {
+
+	public static void main(String[] args) {
+		int num = 2345;
+		int add = 8;
+		double res=(((num+add)/3)%5)*5;
+		System.out.println("The Final Result is : "+ res);
+
+	}
+	
+
+}
