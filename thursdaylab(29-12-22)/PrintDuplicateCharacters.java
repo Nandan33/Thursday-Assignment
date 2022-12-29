38db6e1e16ac03b5207b1ba29ab6b271cc14c34c
Nandan33 <115688247+Nandan33@users.noreply.github.com>
2022-12-29 14:18:52 +0530

    Add files via upload

---
+++ thursdaylab(29-12-22)/PrintDuplicateCharacters.java
@@ -0,0 +1,38 @@
+package thursdaylab29_12_22;
+
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class PrintDuplicateCharacters {
+  public static void main(String[] args) {
+    // Create a Scanner object to read input from the user
+    Scanner scanner = new Scanner(System.in);
+    
+    //  user to enter a string
+    System.out.print("Enter a string: ");
+    
+    // Read the string from the user
+    String str = scanner.nextLine();
+    
+    // Create a HashSet to store the characters that have been encountered
+    HashSet<Character> set = new HashSet<>();
+    
+    // Initialize an empty string to store the duplicate characters
+    String duplicates = "";
+    
+    // Iterate through the string and add each character to the HashSet
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (set.contains(c)) {
+        // If the character is already in the HashSet, it is a duplicate
+        duplicates += c;
+      } else {
+        set.add(c);
+      }
+    }
+    
+    // Print the duplicate characters
+    System.out.println("Duplicate characters: " + duplicates);
+  }
+}
+
---
+++ thursdaylab(29-12-22)/SumOfDigits.java
@@ -0,0 +1,30 @@
+package thursdaylab29_12_22;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+  public static void main(String[] args) {
+    // Create a Scanner object to read input from the user
+    Scanner scanner = new Scanner(System.in);
+    
+    // Prompt the user to enter an integer
+    System.out.print("Enter an integer: ");
+    
+    // Read the integer from the user
+    int n = scanner.nextInt();
+    
+    // Initialize a variable to store the sum of the digits
+    int sum = 0;
+    
+    // Extract the last digit of the integer and add it to the sum until the integer becomes 0
+    while (n > 0) {
+      int digit = n % 10;
+      sum += digit;
+      n /= 10;
+    }
+    
+    // Print the sum of the digits
+    System.out.println("Sum of digits: " + sum);
+  }
+}
+
